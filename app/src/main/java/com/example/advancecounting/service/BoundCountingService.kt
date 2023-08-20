446b31c5006785dec6eab8f6c9bb61484755344a
rakadin <hoangtranminhduc1005>
2023-08-20 17:13:39 +0700

    add bound service using broadcast receiver to update UI

---
+++ app/src/main/java/com/example/advancecounting/service/BoundCountingService.kt
@@ -0,0 +1,53 @@
+package com.example.advancecounting.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.Binder
+import android.os.Handler
+import android.os.IBinder
+import android.os.Message
+import java.lang.ref.WeakReference
+
+class BoundCountingService : Service() {
+    private val binder = CountingBinder()
+    private lateinit var countHandler: CountHandler
+    private var count = 0
+
+    inner class CountingBinder : Binder() {
+        fun getService(): BoundCountingService = this@BoundCountingService
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        countHandler = CountHandler(this)
+        countHandler.sendEmptyMessage(0) // Start counting
+    }
+
+    override fun onBind(intent: Intent): IBinder? {
+        return binder
+    }
+
+    fun getCount(): Int {
+        return count
+    }
+
+    private class CountHandler(service: BoundCountingService) : Handler() {
+        private val serviceReference: WeakReference<BoundCountingService> = WeakReference(service)
+
+        override fun handleMessage(msg: Message) {
+            val service = serviceReference.get() // Get a reference to the BoundCountingService
+            service?.let { // If the service is not null
+                it.count++ // Increment the count value of the service
+                if (it.count <= 100) { // Check if the count is still within the desired range
+                    it.countHandler.sendEmptyMessageDelayed(0, 1000) // Delay sending a message to itself after 1 second
+
+                    // Notify the activity about the count update
+                    val intent = Intent("COUNT_UPDATED") // Create an intent with action "COUNT_UPDATED"
+                    intent.putExtra("count", it.count) // Add the current count value to the intent
+                    it.sendBroadcast(intent) // Send the broadcast intent
+                }
+            }
+        }
+
+    }
+}
--- app/src/main/java/com/example/advancecounting/service/CountingService.kt
+++ app/src/main/java/com/example/advancecounting/service/CountingService.kt
@@ -15,73 +15,66 @@ class CountingService : Service() {
     private var maxCount = 100
     private var serviceLooper: Looper? = null
     private var serviceHandler: ServiceHandler? = null
-    private var isBoundCheck = false
     private var isForeCheck = false
     private var isBackCheck = false
-    private val binder = CountingBinder()
-    // Binder for clients to communicate with the service
-    inner class CountingBinder : Binder() {
-        fun getService(): CountingService = this@CountingService
-    }
+
     // Handler that receives messages from the thread
     private inner class ServiceHandler(looper: Looper) : Handler(looper) {
 
         override fun handleMessage(msg: Message) {
             // Check if counting should be stopped
-            if (stopCounting) {
+            if (stopCounting == true) {
                 // Counting was stopped, stop the service
                 stopSelf(msg.arg1)
                 return
             }
-            // increment the count value
-            Log.v("check_status","update count before")
-            count++
-            Log.v("check_status","update count after")
-            Log.v("check_status_but","check but: $isBoundCheck")
-            Log.v("check_status_but","check but: $isForeCheck")
-            Log.v("check_status_but","check but: $isBackCheck")
-
-
-            if(isBoundCheck == true){ // use bound service
-                Log.v("check_status","check but: $isBoundCheck")
-
-                // send Broadcast to update the UI
-                val intent = Intent("COUNT_UPDATED")
-                intent.putExtra("count", count)
-                sendBroadcast(intent)
-            }
-            else if(isForeCheck == true){ // use foreground service
+            else{
+                // increment the count value
+                Log.v("check_status","update count before")
+                count++ //100
+                Log.v("check_status","update count after")
                 Log.v("check_status_but","check but: $isForeCheck")
-                val notificationBuilder = NotificationBuilder(applicationContext)
-
-                // Create a simple notification
-                notificationBuilder.createSimpleNotification("Counting up by foreground service", "Now: $count", R.drawable.running)
-                if(count==100){
+                Log.v("check_status_but","check but: $isBackCheck")
+//            if(isBoundCheck == true){ // use bound service
+//                Log.v("check_status","check but: $isBoundCheck")
+//
+//                // send Broadcast to update the UI
+//                val intent = Intent("COUNT_UPDATED")
+//                intent.putExtra("count", count)
+//                sendBroadcast(intent)
+//            }
+                if(isForeCheck == true){ // use foreground service
+                    Log.v("check_status_but","check but: $isForeCheck")
                     val notificationBuilder = NotificationBuilder(applicationContext)
 
                     // Create a simple notification
-                    notificationBuilder.createSimpleNotification("Foreground service complete", "$count Counting complete!!!", R.drawable.running)
+                    notificationBuilder.createSimpleNotification("Counting up by foreground service", "Now: $count", R.drawable.running)
+                    if(count==100){
+                        val notificationBuilder = NotificationBuilder(applicationContext)
+
+                        // Create a simple notification
+                        notificationBuilder.createSimpleNotification("Foreground service complete", "$count Counting complete!!!", R.drawable.running)
+                    }
+                }
+                else if(isBackCheck == true){// use background service
+                    Log.v("check_status","check but: $isBackCheck")
+                    if(count==100){
+                        val notificationBuilder = NotificationBuilder(applicationContext)
+
+                        // Create a simple notification
+                        notificationBuilder.createSimpleNotification("Background service complete", "$count Counting complete!!!", R.drawable.running)
+                    }
+                }
+
+                // Continue counting until reaching the maximum count
+                if (count < maxCount) {
+                    // Send a delayed message to self to continue counting
+                    sendEmptyMessageDelayed(0, 1000) // Count every 1 second
+                } else {
+                    // Counting completed, stop the service
+                    stopSelf(msg.arg1)
                 }
             }
-            else if(isBackCheck == true){// use background service
-                Log.v("check_status","check but: $isBackCheck")
-                if(count==100){
-                    val notificationBuilder = NotificationBuilder(applicationContext)
-
-                    // Create a simple notification
-                    notificationBuilder.createSimpleNotification("Background service complete", "$count Counting complete!!!", R.drawable.running)
-                }
-            }
-
-            // Continue counting until reaching the maximum count
-            if (count < maxCount) {
-                // Send a delayed message to self to continue counting
-                sendEmptyMessageDelayed(0, 1000) // Count every 1 second
-            } else {
-                // Counting completed, stop the service
-                stopSelf(msg.arg1)
-            }
-
         }
     }
 
@@ -103,7 +96,7 @@ class CountingService : Service() {
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
         Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show()
         Log.v("check_status","start command service")
-        isBoundCheck = intent.getBooleanExtra("bound_called", false)
+//        isBoundCheck = intent.getBooleanExtra("bound_called", false)
         isForeCheck = intent.getBooleanExtra("fore_called", false)
         isBackCheck = intent.getBooleanExtra("back_called", false)
 
@@ -120,8 +113,9 @@ class CountingService : Service() {
         return START_STICKY
     }
     override fun onBind(intent: Intent): IBinder? {
+        Log.v("check_status","binding in process")
         //("Return the communication channel to the service.")
-        return binder // null if dont provide binding
+        return null// null if dont provide binding
     }
 
 
